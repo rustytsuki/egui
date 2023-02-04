733b12c08ca660b5eceab528945b594075363c3a
RustyTsuki <rustytsuki@gmail.com>
2023-02-04 14:45:10 +0800

    in macos should use skia BGRA8888 color type

--- crates/eframe/src/native/run/skia_cpu_context.rs
+++ crates/eframe/src/native/run/skia_cpu_context.rs
@@ -1,4 +1,5 @@
-use skia_safe::{Surface, ImageInfo, ColorType, AlphaType, wrapper::ValueWrapper};
+use skia_bindings::SkPixmap;
+use skia_safe::{Surface, ImageInfo, ColorType, AlphaType, wrapper::ValueWrapper, Pixel, Handle};
 use softbuffer::GraphicsContext;
 use winit::window::Window;
 
@@ -13,7 +14,8 @@ impl SkiaCPUWindowContext {
     pub fn new(winit_window: &Window, native_options: &epi::NativeOptions) -> Self {
         let graphics_context = unsafe { GraphicsContext::new(&winit_window, &winit_window) }.unwrap();
         let (width, height): (i32, i32) = winit_window.inner_size().into();
-        let image_info = ImageInfo::new((width, height), ColorType::N32, AlphaType::Premul, None);
+        let corlor_type = if cfg!(target_os = "macos") { ColorType::BGRA8888 } else { ColorType::RGBA8888 };
+        let image_info = ImageInfo::new((width, height), corlor_type, AlphaType::Premul, None);
         let surface = Surface::new_raster(&image_info, None, None).unwrap();
         Self {
             surface,
@@ -25,14 +27,26 @@ impl SkiaCPUWindowContext {
     }
 
     pub fn swap_buffers(&mut self) {
-        self.surface.flush();
-
         let pixmap = self.surface.peek_pixels().unwrap();
-        let pixels: &[u32] = pixmap.pixels().unwrap();
+        let pixels: &[u32] = Self::pixels_to_u32(&pixmap).unwrap();
         
         let width = pixmap.width();
         let height = pixmap.height();
 
-        self.graphics_context.set_buffer(&pixels, width as u16, height as u16);
+        self.graphics_context.set_buffer(pixels, width as u16, height as u16);
+    }
+
+    pub fn pixels_to_u32<P: Pixel>(pixmap: &Handle<SkPixmap>) -> Option<&[P]> {
+        let addr = unsafe { pixmap.addr() };
+
+        let info = pixmap.info();
+        let pixel_size = std::mem::size_of::<P>();
+
+        if info.bytes_per_pixel() == pixel_size {
+            let len = pixmap.compute_byte_size() / pixel_size;
+            return Some(unsafe { std::slice::from_raw_parts(addr as *const P, len) });
+        }
+
+        None
     }
 }
